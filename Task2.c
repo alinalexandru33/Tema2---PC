9c764216b33c24ff1593bbff5289018484b722a8
alinalexandru33 <alin.turcitu16@gmail.com>
2022-09-25 01:57:57 +0300

    Task 2.1 Finished

--- Task2.c
+++ Task2.c
@@ -1,6 +1,54 @@
 #include "utils.h"
 
+/* Function to rotate for Caesar encription */
+char rotate(char S, int leftMargin, int rightMargin, int K) {
+    int i;
+
+    for (i = 0; i < K; i++) {
+        S--;
+
+        if (S < leftMargin) {
+            S = rightMargin;
+        }
+    }
+
+    return S;
+}
+
+/* Function user to decript using Caesar */
+void caesar(int K, char S[100]) {
+    int i, leftMargin, rightMargin;
+
+    for (i = 0; i < strlen(S); i++) {
+
+        /* Number */
+        if (S[i] >= 48 && S[i] <= 57) {
+            leftMargin = 48; rightMargin = 57;
+
+        /* Upper case letter */
+        } else if (S[i] >= 65 && S[i] <= 90) {
+            leftMargin = 65; rightMargin = 90;
+
+        /* Lower case letter */
+        } else if (S[i] >= 97 && S[i] <= 122) {
+            leftMargin = 97; rightMargin = 122;
+        }
+
+        S[i] = rotate(S[i], leftMargin, rightMargin, K);
+    }
+}
+
 void SolveTask2() {
-    // TODO
-    printf("Alin\n");
+    int K;
+    char S[100], method[10];
+
+    scanf("%s\n", method);
+    scanf("%d\n", &K);
+    scanf("%s\n", S);
+
+    if (strcmp(method, "caesar") == 0) {
+        caesar(K, S);
+    }
+
+    printf("%s\n", S);
 }
